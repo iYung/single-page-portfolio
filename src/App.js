81e106dc1e2c6e46d07a74000c909ef47454965d
iYung <ivankhyung@gmail.com>
2019-03-19 14:26:34 -0400

    added rest of projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { Carousel, Box, Button, Heading, Grommet, Image, Text, Accordion, AccordionPanel, Menu } from 'grommet';
+import { ResponsiveContext, Carousel, Box, Button, Heading, Grommet, Image, Text, Accordion, AccordionPanel, Menu } from 'grommet';
 import { LinkedinOption, Github, DocumentText } from 'grommet-icons';
 
 const theme = {
   global: {
     colors: {
       brand: '#FFF',
-      text: '#000'
+      text: '#000',
+      control: '#000'
+    },
+    focus: {
+      border: {
+        color: "rgba(0, 0, 0, 0)"
+      }
     },
     font: {
       family: 'Roboto',
@@ -32,96 +38,38 @@ const AppBar = (props) => (
 );
 
 const Images = (props) => (
-  <Box 
-    flex 
-    background="brand"
-    overflow='hidden'
-    align='center' 
-    justify='center'
-    height="medium"
-    margin={{left: "large", right: "large"}}
-  >
-    <Carousel fill>
-      <Image
-        fit="cover"
-        alignSelf="center"
-        src="https://scontent.fyyz1-1.fna.fbcdn.net/v/t31.0-8/15002366_1390047507702480_3399042522196563818_o.jpg?_nc_cat=105&_nc_ht=scontent.fyyz1-1.fna&oh=75118d3c1e9177f277a9f0db625dd69d&oe=5D0CE952"
-      />
-      <Image
-        fit="cover"
-        alignSelf="center"
-        src="https://scontent.fyyz1-1.fna.fbcdn.net/v/t31.0-8/16179805_1353691424681532_1863141769273328124_o.jpg?_nc_cat=107&_nc_ht=scontent.fyyz1-1.fna&oh=abcadb245bfb2c081e19b09813aa083e&oe=5D051D42"
-      />
-      <Image
-        fit="cover"
-        alignSelf="center"
-        src="https://scontent.fyyz1-1.fna.fbcdn.net/v/t31.0-8/16179717_1353697871347554_4799742379663058865_o.jpg?_nc_cat=102&_nc_ht=scontent.fyyz1-1.fna&oh=df5db04ca1804efe9beede15a028cf5e&oe=5D4FD07A"
-      />
-    </Carousel>
-  </Box>
+  <ResponsiveContext.Consumer>
+    {(size) => (
+      <Box 
+        flex 
+        background="brand"
+        overflow='hidden'
+        align='center' 
+        justify='center'
+        height={size === "small" ? "medium" :"large"}
+        margin={{left: "large", right: "large"}}
+      >
+        <Carousel fill>
+          <Image
+            fit="cover"
+            alignSelf="center"
+            src={require("./0.png")}
+          />
+          <Image
+            fit="cover"
+            alignSelf="center"
+            src={require("./1.png")}
+          />
+          <Image
+            fit="cover"
+            alignSelf="center"
+            src={require("./2.png")}
+          />
+        </Carousel>
+      </Box>
+    )}
+  </ResponsiveContext.Consumer>
 );
-
-// const Work = (props) => (
-//   <Box
-//     width="large"
-//     align="center"
-//   >
-//     <ResponsiveContext.Consumer>
-//     {(size) => {
-//       if (size == "small") {
-//         return (
-//           <Grid
-//             rows={['xsmall', 'xsmall']}
-//             columns={['medium']}
-//             gap="large"
-//             areas={[
-//               { name: 'header', start: [0, 0], end: [0, 0] },
-//               { name: 'nav', start: [0, 1], end: [0, 1] },
-//             ]}
-//           >
-//             <Box gridArea="header" background="brand">
-//               <Image
-//                   fit="contain"
-//                   src="https://www.workintech.ca/images/111/default/td-bank-financial-group_logo_201702281442012.png"
-//               />
-//             </Box>
-//             <Box gridArea="nav" background="brand">
-//               <Image
-//                   fit="contain"
-//                   src="https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Celestica_logo.svg/1200px-Celestica_logo.svg.png"
-//               />
-//             </Box>
-//           </Grid>
-//         )
-//       }
-//       return (
-//         <Grid
-//           rows={['small']}
-//           columns={['medium', 'medium']}
-//           gap="large"
-//           areas={[
-//             { name: 'header', start: [0, 0], end: [0, 0] },
-//             { name: 'nav', start: [1, 0], end: [1, 0] },
-//           ]}
-//         >
-//           <Box gridArea="header" background="brand">
-//             <Image
-//                 fit="contain"
-//                 src="https://www.workintech.ca/images/111/default/td-bank-financial-group_logo_201702281442012.png"
-//             />
-//           </Box>
-//           <Box gridArea="nav" background="brand">
-//             <Image
-//                 fit="contain"
-//                 src="https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Celestica_logo.svg/1200px-Celestica_logo.svg.png"
-//             />
-//           </Box>
-//         </Grid>
-//       )
-//     }}
-//   </ResponsiveContext.Consumer>
-//   </Box>
-// )
 
 const Work = (props) => (
   <Box width="large">
@@ -311,6 +259,33 @@ class Projects extends React.Component {
               </Box>
             </Box>
             <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">genome meetup</Text>
+              <Text weight="bold">jul 2018</Text>
+              <Text>ios app to organize meetups with genome data</Text>
+              <Text>app built with swift</Text>
+              <Text>back-end made with python & firebase</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/genome-meetup">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">leach</Text>
+              <Text weight="bold">jul 2018</Text>
+              <Text>web application for skill sharing</Text>
+              <Text>front-end made with react & nodejs</Text>
+              <Text>back-end made with nodejs & mongodb</Text>
+              <Box direction="row">
+                <a href="https://devcircles2018.herokuapp.com/">
+                  <Text margin={{right: "small"}}>website</Text>
+                </a>
+                <a href="https://github.com/iYung/fb-community-challenge-2018">
+                  <Text>github</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
               <Text weight="bold">tap atm</Text>
               <Text weight="bold">may 2018</Text>
               <Text>payment experiment using nfc with atms</Text>
@@ -326,11 +301,196 @@ class Projects extends React.Component {
                 </a>
               </Box>
             </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">positivity game</Text>
+              <Text weight="bold">apr 2018</Text>
+              <Text>firefox add-on which promoted internet positivity</Text>
+              <Text>add-on made with javascript</Text>
+              <Text>back-end made with AWS, nodejs & redis</Text>
+              <Box direction="row">
+                <a href="https://github.com/iYung/positivityGame">
+                  <Text>github</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">longstrip for mangadex</Text>
+              <Text weight="bold">jan 2018</Text>
+              <Text>chrome extension which added a long strip reading mode</Text>
+              <Text>hit 1400+ downloads & 1000+ daily users</Text>
+              <Text>add-on made with coffeescript & jquery</Text>
+              <Box direction="row">
+                <a href="https://github.com/iYung/mangaDexAllPagesExtension">
+                  <Text>github</Text>
+                </a>
+              </Box>
+            </Box>
+          </Box>
+        }
+        {
+          this.state.year === "2017" &&
+          <Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">shelfie</Text>
+              <Text weight="bold">nov 2017</Text>
+              <Text>shelf robot capable of fetching items</Text>
+              <Text>desktop app built with windows forms, python & c#</Text>
+              <Text>hardware built with a spark photon & motors</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/shelfie-njv01d">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">portfolio</Text>
+              <Text weight="bold">nov 2017</Text>
+              <Text>wordpress-like web application</Text>
+              <Text>front-end built with react & nodejs</Text>
+              <Text>back-end made with express & nodejs</Text>
+              <Text>deployed on heroku</Text>
+              <Box direction="row">
+                <a href="https://github.com/iYung/portfolio1.5">
+                  <Text margin={{right: "small"}}>github</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">grow with the flow</Text>
+              <Text weight="bold">nov 2017</Text>
+              <Text>iot plant watering solution</Text>
+              <Text>front-end built with react & nodejs</Text>
+              <Text>back-end made with express & nodejs</Text>
+              <Text>hardware made with a raspberry pi & solenoids</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/grow-with-the-flow">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+                <a href="https://github.com/iYung/plantWatering">
+                  <Text margin={{right: "small"}}>github</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">gall-ar-y</Text>
+              <Text weight="bold">sept 2017</Text>
+              <Text>augmented reality app to demo paintings</Text>
+              <Text>front-end built with react & nodejs</Text>
+              <Text>back-end made with express, aws & nodejs</Text>
+              <Text>phone app made with c#, unity & vuforia</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/gall-ar-y">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">atlas reactor replays</Text>
+              <Text weight="bold">sept 2017</Text>
+              <Text>atlas reactor game replay sharing website and desktop client</Text>
+              <Text>front-end built with react & nodejs</Text>
+              <Text>back-end made with express, aws & nodejs</Text>
+              <Text>desktop app made with c# & windows forms</Text>
+              <Box direction="row">
+                <a href="https://github.com/iYung/atlasReactorReplays">
+                  <Text margin={{right: "small"}}>github</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">green waste bin</Text>
+              <Text weight="bold">feb 2017</Text>
+              <Text>smart waste bin that can sort</Text>
+              <Text>front-end built with jquery & javascript</Text>
+              <Text>back-end made with indico</Text>
+              <Text>hardware made with a raspberry pi, motors & python</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/green-waste-bin">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+                <a href="https://github.com/pwnedpixel/GreenGarbage">
+                  <Text margin={{right: "small"}}>github</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">theia</Text>
+              <Text weight="bold">jan 2017</Text>
+              <Text>smart waste bin that can sort</Text>
+              <Text>front-end built with azure & js</Text>
+              <Text>back-end made with azure & c#</Text>
+              <Text>hardware made with a raspberry pi & python</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/theia-o02nk8">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+                <a href="https://github.com/pwnedpixel/RecogToSpeech">
+                  <Text margin={{right: "small"}}>github</Text>
+                </a>
+              </Box>
+            </Box>
+          </Box>
+        }
+        {
+          this.state.year === "2016" &&
+          <Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">braille printer</Text>
+              <Text weight="bold">oct 2016</Text>
+              <Text>low cost braille printer</Text>
+              <Text>hardware built with a raspberry pi, python & solenoids</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/braille-printer">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+                <a href="https://github.com/pwnedpixel/BraillePrinter">
+                  <Text margin={{right: "small"}}>github</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">ar watch</Text>
+              <Text weight="bold">mar 2016</Text>
+              <Text>google cardboard augmented reality watch</Text>
+              <Text>phone app made with unity, c# & vuforia</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/ar-watch">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+              </Box>
+            </Box>
+            <Box margin={{bottom: "medium"}}>
+              <Text weight="bold">shake and quake</Text>
+              <Text weight="bold">mar 2016</Text>
+              <Text>earthquake data visualization with hardware</Text>
+              <Text>hardware made with a spark photon & leds</Text>
+              <Box direction="row">
+                <a href="https://devpost.com/software/shake-and-quake-7we4zv">
+                  <Text margin={{right: "small"}}>devpost</Text>
+                </a>
+              </Box>
+            </Box>
+          </Box>
+        }
+        {
+          this.state.year === "2015" &&
+          <Box margin={{bottom: "medium"}}>
+            <Text weight="bold">just out for a rift</Text>
+            <Text weight="bold">nov 2015</Text>
+            <Text>vr biking experience</Text>
+            <Text>vr app built with unity & c#</Text>
+            <Text>hardware built with an arduino, oculus rift & exercise bike</Text>
+            <Box direction="row">
+              <a href="https://devpost.com/software/just-out-for-a-rift">
+                <Text margin={{right: "small"}}>devpost</Text>
+              </a>
+            </Box>
           </Box>
         }
       </Box>
     )
   }
+  
 }
 
 const Articles = (props) => (
