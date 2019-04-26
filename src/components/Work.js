4056d9b0117a41e585b5a9bb3a4bf33f1e939ab9
iYung <ivankhyung@gmail.com>
2019-04-26 02:18:45 -0400

    updated work text

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,6 +4,11 @@ import { Box, Text } from 'grommet';
 const Work = (props) => (
     <Box width="large">
       <Box margin={{bottom: "medium"}}>
+        <Text weight="bold" margin={{right: "xsmall"}}>software engineer @ square</Text>
+        <Text weight="bold">starting july 2019</Text>
+        <Text>working on cash app</Text>
+      </Box>
+      <Box margin={{bottom: "medium"}}>
         <Text weight="bold" margin={{right: "xsmall"}}>developer co-op @ td innovation lab</Text>
         <Text weight="bold">may 2018 to aug 2018</Text>
         <Text>mobile app development for android & ios</Text>
